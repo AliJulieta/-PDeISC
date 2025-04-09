9c864cc830e6c38e0ae98edd6169af004e915466
AliJulieta <julietaali851@gmail.com>
2025-04-09 20:53:26 -0300

    trabajo5.js

---
+++ trabajo5.js
@@ -0,0 +1,13 @@
+!DOCTYPE html>
+ <html lang="es">
+     <head>
+         <meta charset="UTF-8">
+          <title>Resultados de operaciones</title> <style>
+             table { width: 50%; margin: 50px auto; 
+             border-collapse: collapse;
+              font-family: Arial, sans-serif;
+               } th, td { border: 1px solid #444; padding: 10px; text-align: center;
+                } th { background-color: #333;
+                 color: white; } tr:nth-child(even) { background-color: #eee; } 
+                 
+                 </style> </head> <body> <table> <tr> <th>Operación</th> <th>Resultado</th> </tr> <tr><td>Suma 5 + 3</td><td>8</td></tr> <tr><td>Resta 8 - 6</td><td>2</td></tr> <tr><td>Multiplicación 3 * 11</td><td>33</td></tr> <tr><td>División 30 / 5</td><td>6</td></tr> </table> </body> </html>
