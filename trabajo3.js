54cb108ee9bdfd1b5e61c7045b4f8d9fb1b65378
AliJulieta <julietaali851@gmail.com>
2025-04-09 20:50:50 -0300

    trabajo3.js

---
+++ trabajo3.js
@@ -0,0 +1,21 @@
+// ejercicio3.js
+//  // Repite las operaciones del ejercicio 2 usando funciones definidas por el usuario 
+function sumar(a, b)
+ { return a + b; }
+
+  function restar(a, b) {
+  return a - b; 
+}
+    function multiplicar(a, b){ 
+     return a * b; } 
+
+     function dividir(a, b) {
+         return a / b; }
+//se muestra por pantalla el resutado del codigo 
+          console.log("Suma: ", sumar(4, 5)); 
+          console.log("Resta: ", restar(3, 6));
+           console.log("Multiplicación: ", multiplicar(2, 7));
+            console.log("División: ", dividir(20, 4));
+
+
+
